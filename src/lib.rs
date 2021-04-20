2330c587b498a1a22adccb6769352b8c15fc9025
snakamura <snakamura@vision.ist.i.kyoto-u.ac.jp>
2021-04-20 22:26:52 +0900

    Implement 6.11.

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,25 @@ pub mod vec3;
 use ray::Ray;
 use vec3::{color::Color, point::Point3};
 
-fn hit_sphere(center: Point3, radius: f64, r: Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f64, r: Ray) -> f64 {
     let oc = r.origin() - center;
     let a = r.direction().length_squared();
     let b = 2.0 * r.direction().dot(&oc);
     let c = oc.length_squared() - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.
+    if discriminant < 0. {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt()) / (2.0 * a)
+    }
 }
 
 pub fn ray_color(r: Ray) -> Color {
-    if hit_sphere(Point3::new([0., 0., -1.]), 0.5, r.clone()) {
-        return Color::new([1., 0., 0.]);
+    let center_of_the_sphere = Point3::new([0., 0., -1.]);
+    let t = hit_sphere(center_of_the_sphere.clone(), 0.5, r.clone());
+    if t > 0.0 {
+        let n = (r.at(t) - center_of_the_sphere).normalize();
+        return Color::new([n.x() + 1.0, n.y() + 1.0, n.z() + 1.0]) * 0.5;
     }
     let unit_direction = r.direction().normalize();
     let t = (unit_direction.y() + 1.0) * 0.5;

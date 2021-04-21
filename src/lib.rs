9bedafdd431ae3e281c4caa4fba17c93de790b10
snakamura <snakamura@vision.ist.i.kyoto-u.ac.jp>
2021-04-21 22:44:24 +0900

    Implement 10.36.

--- src/lib.rs
+++ src/lib.rs
@@ -68,6 +68,11 @@ pub fn render() {
     world.add(Rc::new(Sphere::new(
         Point3::new([-1.0, 0.0, -1.0]),
         0.5,
+        material_left.clone(),
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point3::new([-1.0, 0.0, -1.0]),
+        -0.4,
         material_left,
     )));
     world.add(Rc::new(Sphere::new(
